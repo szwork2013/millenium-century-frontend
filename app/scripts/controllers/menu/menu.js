cf02457f12c9149eb55d0e8967ef47b861658b78
Geiv Tomco <geiv.tomco@comm-it.it>
2015-10-16 19:21:58 +0200

    Made dynamic navbar

--- app/scripts/controllers/menu/menu.js
+++ app/scripts/controllers/menu/menu.js
@@ -1,6 +1,13 @@
 ﻿'use strict';
 angular.module('millenium.controllers')
-  .controller('menuController', function ($scope, authorizationService, identityStoreService) {
+  .controller('menuController', function ($scope, authorizationService, identityStoreService, resourceService) {
+	
+	resourceService.forResource('menuItems')
+		.get()
+			.then(function(result){
+				$scope.menuItems = result.menuItems;
+			});
+	
 
     $scope.authorizationService = authorizationService;
     $scope.identity = function () {
